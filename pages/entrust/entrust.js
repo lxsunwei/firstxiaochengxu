8d4299b2fe1a6232673014c2b430189438d2122f
sunwei <lx_sunwei@163.com>
2018-05-09 17:15:58 +0800

    text

--- pages/entrust/entrust.js
+++ pages/entrust/entrust.js
@@ -6,8 +6,12 @@ Page({
    */
   data: {
     isBoy: true,
-    pcode: 2365
-
+    isGirl: false,
+    pcode: 2365,
+    identity: "上班族",
+    identites: ["上班族", "企业主", "个体户", "其他"],
+    amount: "10万",
+    amounts: ["5万", "10万", "20万", "30万", "40万", "80万"]
   },
   login: function () {
     wx.login({
@@ -32,10 +36,20 @@ Page({
       url: '../../pages/protocol/protocol',
     })
   },
-  randomCode: function() {
+  randomCode: function () {
     var num = Math.floor(Math.random() * 10000);
     this.setData({
-        pcode: num
+      pcode: num
+    });
+  },
+  selectIdentity: function (e) {
+    this.setData({
+      identity: this.data.identites[e.detail.value]
+    });
+  },
+  selectLoanAmount: function (e) {
+    this.setData({
+      amount: this.data.amounts[e.detail.value]
     });
   }
 })
