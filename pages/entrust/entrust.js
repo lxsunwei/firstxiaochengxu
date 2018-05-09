ab4c06c3019afe7e9fdcf769b47ed25ce48de846
sunwei <lx_sunwei@163.com>
2018-05-09 15:52:42 +0800

    icon

--- pages/entrust/entrust.js
+++ pages/entrust/entrust.js
@@ -5,18 +5,37 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    isBoy: true,
+    pcode: 2365
+
   },
-  login: function() {
+  login: function () {
     wx.login({
-      success : function(res) {
-        if(res.code) {
+      success: function (res) {
+        if (res.code) {
           wx.showToast({
             title: res.code,
           })
         }
       }
-      
+
     })
+  },
+  changeSex: function () {
+    this.setData({
+      isBoy: !this.data.isBoy,
+      isGirl: !this.data.isGirl
+    });
+  },
+  lookProtocol: function () {
+    wx.navigateTo({
+      url: '../../pages/protocol/protocol',
+    })
+  },
+  randomCode: function() {
+    var num = Math.floor(Math.random() * 10000);
+    this.setData({
+        pcode: num
+    });
   }
 })
